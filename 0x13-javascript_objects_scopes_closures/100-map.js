5b7c5ad6fd2e42c2d36f36ecc3699c859610d4b3
Dechasa Shimels <59132317+dechasa12@users.noreply.github.com>
2023-03-15 20:50:44 +0300

    Task 11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
-const arr = require('./100-data').list;
-
-console.log(arr);
-console.log(arr.map((x, idx) => x * idx));
+const oldArr = require('./100-data').list;
+console.log(oldArr);
+const newArr = oldArr.map((el, i) => el * i);
+console.log(newArr);
